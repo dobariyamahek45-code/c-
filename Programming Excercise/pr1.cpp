124b7198901e37b00fae7df4521fbcd93633dab0
mahek dobariya <mahekdobariya45@gmail.com>
2026-05-24 14:05:34 +0530

    new folder

---
+++ Programming Excercise/pr1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int x=10;
+	int y=5;
+		
+	cout<<"addtion := "<<x+y<<endl;
+	cout<<"substraction := "<<x-y<<endl;
+	cout<<"mutipaction := "<<x*y<<endl;
+	cout<<"division := "<<x/y<<endl;
+	return 0;
+}
